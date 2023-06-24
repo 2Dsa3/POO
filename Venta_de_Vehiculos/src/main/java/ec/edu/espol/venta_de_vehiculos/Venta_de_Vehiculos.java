6f70d29f3fcb80f5ccd413159c3e302f97a317ce
abigail wdkb <davsum04@hotmail.com>
2023-06-24 15:34:43 -0500

    ya esta subida la carpeta

---
+++ Venta_de_Vehiculos/src/main/java/ec/edu/espol/venta_de_vehiculos/Venta_de_Vehiculos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.venta_de_vehiculos;
+
+/**
+ *
+ * @author abisu
+ */
+public class Venta_de_Vehiculos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
