6d0b760d8406722ce6d8773ea14432db9bd4f5cf
Alex <licazago@fiec.espol.edu.ec>
2023-08-19 15:19:01 -0500

    prueba

--- ajedrezProyecto/src/main/java/ec/edu/espol/ajedrezproyecto/App.java
+++ ajedrezProyecto/src/main/java/ec/edu/espol/ajedrezproyecto/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ec.edu.espol.controllers.TableroAjedrezController;
 
 import java.io.IOException;
 
