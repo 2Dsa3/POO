6bd00205a6178a6fac5df1cbea8d3362617535da
Alex <licazago@fiec.espol.edu.ec>
2023-08-19 17:44:14 -0500

    ahora si funciona

--- ajedrezProyecto/src/main/java/ec/edu/espol/classes/Tablero.java
+++ ajedrezProyecto/src/main/java/ec/edu/espol/classes/Tablero.java
@@ -11,20 +11,20 @@ import java.util.ArrayList;
  * @author abisu
  */
 public class Tablero {
-    private Ficha[][] fichas;
-
-    public Tablero() {
-        this.fichas = new Ficha[8][8];
-    }
-    public void llenarTablero(){
-        for (int i = 0; i < 8; i++) {
-            fichas[1][i]= new Pawn(0,casi,"black",1,i);
-        }
-        for (int i = 0; i < 8; i++) {
-            fichas[6][i]= new Pawn(0,casi,"white",6,i);
-        }
-    }
-         
+//    private Ficha[][] fichas;
+//
+//    public Tablero() {
+//        this.fichas = new Ficha[8][8];
+//    }
+//    public void llenarTablero(){
+//        for (int i = 0; i < 8; i++) {
+//            fichas[1][i]= new Pawn(0,casi,"black",1,i);
+//        }
+//        for (int i = 0; i < 8; i++) {
+//            fichas[6][i]= new Pawn(0,casi,"white",6,i);
+//        }
+//    }
+//         
     
     
 }
--- ajedrezProyecto/src/main/java/module-info.java
+++ ajedrezProyecto/src/main/java/module-info.java
@@ -5,4 +5,7 @@ module ec.edu.espol.ajedrezproyecto {
 
     opens ec.edu.espol.ajedrezproyecto to javafx.fxml;
     exports ec.edu.espol.ajedrezproyecto;
+    opens ec.edu.espol.controllers to javafx.fxml;
+    exports ec.edu.espol.controllers;
+    
 }
