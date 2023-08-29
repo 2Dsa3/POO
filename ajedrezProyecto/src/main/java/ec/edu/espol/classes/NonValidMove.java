835d5e6721faf47cf5893cc3936f528918cc5ed9
Jorge Apolo <japolo15@MacBook-Pro-de-gabriela.local>
2023-08-29 13:09:12 -0500

    Forgot to commit Exception

---
+++ ajedrezProyecto/src/main/java/ec/edu/espol/classes/NonValidMove.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.classes;
+
+/**
+ *
+ * @author japolo15
+ */
+public class NonValidMove extends Exception{
+
+    public NonValidMove(String message) {
+        super(message);
+    }
+    
+}
