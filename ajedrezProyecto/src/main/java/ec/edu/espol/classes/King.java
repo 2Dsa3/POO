7a7da3a0076875a4bcf0966e8e41d8ea532143a8
abigail wdkb <davsum04@hotmail.com>
2023-09-03 16:52:59 -0500

    newclass

--- ajedrezProyecto/src/main/java/ec/edu/espol/classes/King.java
+++ ajedrezProyecto/src/main/java/ec/edu/espol/classes/King.java
@@ -44,4 +44,26 @@ public class King extends Ficha{
         //Esto es para trabajar en las finales
         return false;
     }
+    //color es el del rey
+    public boolean estaEnJaque(String color){
+        boolean b;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (! (t.fichas[i][j].getColor().equals(Equipo.valueOf(color)))){
+                    try{
+                        t.fichas[i][j].capturar(this);
+                        
+                    }
+                    catch(NonValidMove e){
+                        
+                    }
+                    catch (PossibleCheckmate ex){
+                        
+                    }
+                }
+                    
+            }
+        }
+        return false;
+    }
 }
---
+++ ajedrezProyecto/src/main/java/ec/edu/espol/classes/PossibleCheckmate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.classes;
+
+/**
+ *
+ * @author abisu
+ */
+public class PossibleCheckmate extends Exception{
+
+    public PossibleCheckmate(String message) {
+        super(message);
+    }
+}
