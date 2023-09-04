096d4b664f0f675a7fcf4ab129a140ba48b40bd4
abigail wdkb <davsum04@hotmail.com>
2023-09-04 17:28:05 -0500

    sh

--- ajedrezProyecto/src/main/java/ec/edu/espol/controllers/CoronacionController.java
+++ ajedrezProyecto/src/main/java/ec/edu/espol/controllers/CoronacionController.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 /**
@@ -45,6 +46,22 @@ public class CoronacionController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        this.cargarImagenes();
     }    
+    
+    public void cargarImagenes(){
+        
+        Image imgA = new Image("ec/edu/espol/chessPieces/blackpawn.png");
+        Image imgB = new Image("ec/edu/espol/chessPieces/torre_real.jpg");
+        Image imgC = new Image("ec/edu/espol/chessPieces/caballo_real.jpg");
+        Image imgD = new Image("ec/edu/espol/chessPieces/reina_real.jpg");
+        
+        ImgAlfil= new ImageView(imgA);
+        ImgTorre= new ImageView(imgB);
+        ImgCaballo= new ImageView(imgC);
+        ImgReina= new ImageView(imgD);
+    }
+            
     
 }
